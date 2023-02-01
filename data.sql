6f248a1f5c915cabc53c1cebd0af4c64f11ab5db
iya30n <varmeziyaryasin@gmail.com>
2023-02-01 18:21:46 +0330

    update sql data file

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
 create database simpleapp;
 use simpleapp;
 create table admins (id int not null auto_increment, name varchar(255) not null, username varchar(255) not null, password varchar(255) not null, primary key(id));
-insert into admins (name, username, password) values("dev", "dev", "1234");
+insert into admins (name, username, password) values("dev", "dev", "$2a$14$mIa2HZeeeEoKN94JxnX...ok1F6RGtQfMnSYMoc.oLoNL5c2/nkQa"); -- password is 1111
