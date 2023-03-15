d962443a5b35ba49d2cfbd3505f02fe76ddbc7f3
iya30n <varmeziyaryasin@gmail.com>
2023-03-15 13:26:29 +0330

    use response handler on admins list

--- app/controllers/admin/AdminController/List.go
+++ app/controllers/admin/AdminController/List.go
@@ -1,16 +1,13 @@
 package AdminController
 
 import (
-	"encoding/json"
 	"net/http"
 	"simpleapp/app/models/Admin"
+	responsehandler "simpleapp/core/responseHandler"
 )
 
 func List(w http.ResponseWriter, r *http.Request) {
 	adminsList, _ := Admin.All()
 
-	err := json.NewEncoder(w).Encode(adminsList)
-	if err != nil {
-		panic(err.Error())
-	}
+	responsehandler.Json(w, adminsList, http.StatusOK)
 }
