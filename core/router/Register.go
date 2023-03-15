a09e34d5fc047febd381feaaf1a9f8282cd729b4
iya30n <varmeziyaryasin@gmail.com>
2023-03-15 16:56:01 +0330

    support more http methods

--- core/router/Register.go
+++ core/router/Register.go
@@ -5,21 +5,25 @@ import (
 )
 
 func Get(route string, controllerMethod http.HandlerFunc) *RouteType {
-	routeHandler := RouteType{
-		Url:        route,
-		HttpMethod: "GET",
-		Callable:   controllerMethod,
-	}
-
-	routes = append(routes, routeHandler)
-
-	return &routeHandler
+	return register(route, "GET", controllerMethod)
 }
 
 func Post(route string, controllerMethod http.HandlerFunc) *RouteType {
+	return register(route, "POST", controllerMethod)
+}
+
+func Put(route string, controllerMethod http.HandlerFunc) *RouteType {
+	return register(route, "PUT", controllerMethod)
+}
+
+func Delete(route string, controllerMethod http.HandlerFunc) *RouteType {
+	return register(route, "DELETE", controllerMethod)
+}
+
+func register(route string, method string, controllerMethod http.HandlerFunc) *RouteType {
 	routeHandler := RouteType{
 		Url:        route,
-		HttpMethod: "POST",
+		HttpMethod: method,
 		Callable:   controllerMethod,
 	}
 
