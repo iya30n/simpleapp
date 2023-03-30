0eba64d34216d45a1ec9c17dfea693dc7056cf91
yasin <yascanary0@gmail.com>
2023-03-30 15:22:54 +0330

    apply middlewares for routes

--- main.go
+++ main.go
@@ -1,41 +1,19 @@
 package main
 
 import (
-	"net/http"
 	"simpleapp/app/controllers/admin/AdminController"
 	"simpleapp/app/controllers/admin/AuthController"
-	"simpleapp/app/modules/jwtHandler"
-	"simpleapp/core/responseHandler"
+	"simpleapp/app/middlewares"
+	"simpleapp/core/router"
 )
 
-func authMiddleware(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if len(token) < 1 || !jwtHandler.Verify(token) {
-			responsehandler.Json(w, map[string]string{"message": "UnAuthenticated!"}, http.StatusBadRequest)
-
-			return
-		}
-
-		fn(w, r)
-	}
-}
-
 func main() {
-	http.HandleFunc("/admin/admins", authMiddleware(AdminController.List))
-	http.HandleFunc("/admin/admins/edit/", authMiddleware(AdminController.Update))
+	router.Get("/admin/admins", AdminController.List).Middlewares(middlewares.Auth{})
+	router.Post("/admin/admins/edit/", AdminController.Update).Middlewares(middlewares.Auth{})
 
-	http.HandleFunc("/admin/login", AuthController.Login)
-	http.HandleFunc("/admin/register", authMiddleware(AuthController.Register))
-	http.HandleFunc("/admin/refresh-token", AuthController.Refresh)
+	router.Post("/admin/login", AuthController.Login)
+	router.Post("/admin/register", AuthController.Register).Middlewares(middlewares.Auth{})
+	router.Post("/admin/refresh-token", AuthController.Refresh)
 
-	http.ListenAndServe(":9090", nil)
+	router.Serve("9090")
 }
-
-/*
-TODO:
-	[] write tests
-	[] validation handler system
-	[] routing system
-	[] middleware handler on routing system
-*/
